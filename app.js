2f5022561b4476e2b10aab035d12d9b2b57c9d03
Ricardo2930 <rhuaytan@gmail.com>
2023-08-08 15:34:24 -0500

    updated with bootstrap

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ const getData = async () => {
   const user = document.getElementById("users")
   const names = user.value
   const response = await fetch(`https://api.github.com/users/${names}`)
-  //console.log(response)
   const data = await response.json()
-  //console.log(data)
 
-  container.innerHTML = ""
   const { name, bio, avatar_url, followers, following, public_repos, html_url } = data
-  container.innerHTML +=
+  
+  container.innerHTML =
     `
-      <div class="card">
-        <img src="${avatar_url}" alt="avatar">
+        <div>
+          <img src="${avatar_url}" alt="avatar">
+        </div>
+
         <div class="card-body">
           <h3>${name}</h3>
           <p>${bio}</p>
         </div>
+
         <div class="seguidores">
-          <h5>Followers<br>${followers}</h5>
-          <h5>Following<br>${following}</h5>
-          <h5>Repositories<br>${public_repos}</h5>
+          <p>Followers<br>${followers}</p>
+          <p>Following<br>${following}</p>
+          <p>Repositories<br>${public_repos}</p>
         </div>
+
         <div class="github">
-        <a class="fcc-btn" id="iconogit" href="${html_url}" target="_blank">
-        <i class="fa-brands fa-github" id="icono"></i> 
-        GitHub</a>
+          <a class="fcc-btn" id="iconogit" href="${html_url}" target="_blank">
+          <i class="fa-brands fa-github" id="icono"></i> GitHub</a>
         </div>   
-      </div>
     `
 }
 
